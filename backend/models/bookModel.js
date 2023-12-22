6f4439b42630dc4fdcce7feb926dc72278715d00
Poorna Chander Terala <chanduterala24@gmail.com>
2023-12-22 21:21:49 +0530

    added new in model

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const bookSchema = new mongoose.Schema(
   {
     title: {
-      type: String,
       required: true,
+      type: String,
     },
     author: {
       type: String,
